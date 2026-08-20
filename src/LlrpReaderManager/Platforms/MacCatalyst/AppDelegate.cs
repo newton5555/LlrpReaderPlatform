c81387f633795eaa005fa1894e160e022f53a93d
严 凯 <newton5555@hotmail.com>
2026-08-20 21:35:58 +0800

    fix: add mac catalyst application entry point

---
+++ src/LlrpReaderManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+using Foundation;
+using Microsoft.Maui;
+using UIKit;
+
+namespace LlrpReaderManager;
+
+[Register("AppDelegate")]
+public sealed class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
